c428b3f9522a2e852a9a85a7f2da56ab38024b61
Temitope Oke <topeoke@gmail.com>
2013-04-23 21:18:00 +0100

    static-pages-controller-generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleAppV2::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
